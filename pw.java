c3ac381f809c6571b8700686776060bab8f81609
pragatisingh9155 <pragatisingh9155@gmail.com>
2024-08-06 13:47:18 +0530

    pw.java modifies added to main1()

--- pw.java
+++ pw.java
@@ -2,4 +2,8 @@ public class first{
     public static void main(String args[]){
       System.out.print.ln("hello");
 }
+public static void main1(String args[]){
+      System.out.print.ln("hello");
+}
+
 }
